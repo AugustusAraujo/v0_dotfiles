20218c786988942252d8ece33365945838542b10
Augustus Oliveira <augustusaraujo13@gmail.com>
2026-03-29 16:44:42 -0300

    feat: add hyprpanel

--- utils/prevent_killactive.sh
+++ utils/prevent_killactive.sh
@@ -2,8 +2,17 @@
 
 active_window_class=$(hyprctl activewindow -j | jq -r ".title")
 
+# if [ "$active_window_class" = "Steam" ]; then
+#     #echo "Coisou" >> /home/gustin/.config/hypr/close.log
+#     xdotool getactivewindow windowunmap
+# fi
+# if [ "$active_window_class" = "Counter-Strike 2" ]; then
+#     echo "Coisou" >> /home/gustin/v0_dotfiles/close.log
+#     # xdotool getactivewindow windowunmap
+# fi
 if [ "$active_window_class" = "satty" ]; then
   ydotool key 1:1 1:0   # ESC
 else
   hyprctl dispatch killactive ""
 fi
+
